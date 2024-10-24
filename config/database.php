9907af7470ffd1813922945d1b2531d654634517
Carlos William <williamxavier0000@gmail.com>
2024-10-24 19:28:20 -0300

    Configure database to use JawsDB

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,24 +43,20 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+                'driver' => 'mysql',
+                'url' => env('JAWSDB_URL'),
+                'host' => parse_url(env('JAWSDB_URL'), PHP_URL_HOST),
+                'port' => parse_url(env('JAWSDB_URL'), PHP_URL_PORT),
+                'database' => ltrim(parse_url(env('JAWSDB_URL'), PHP_URL_PATH), '/'),
+                'username' => parse_url(env('JAWSDB_URL'), PHP_URL_USER),
+                'password' => parse_url(env('JAWSDB_URL'), PHP_URL_PASS),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
