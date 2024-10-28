0fe679da885b15d0ad48df070b1f4d3840c8c5e7
Carlos William <williamxavier0000@gmail.com>
2024-10-28 11:08:54 -0300

    Alteração Clojjjjudinary

--- config/services.php
+++ config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    'services' => [
+    'cloudinary' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET')
+    ]
+]
+
 ];
