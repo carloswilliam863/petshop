6e767cb3979e59742ecd804c1726283dc9bb8305
Carlos William <williamxavier0000@gmail.com>
2024-10-28 22:14:26 -0300

    Alteração Clojjjjudinary

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,20 +4,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
     public function store(Request $request)
-    {
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
+        {
+            $request->validate([
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
 
-        $uploadedFileUrl = Cloudinary::upload($request->file('image')->getRealPath())->getSecurePath();
-        
+            // Faz o upload do arquivo para o S3
+            $path = Storage::disk('s3')->put('images', $request->file('image'));
 
-        // Salve a URL no banco de dados ou use conforme necessário
-        return redirect()->back()->with('success', 'Imagem enviada com sucesso!');
-    }
+            // Obtenha a URL do arquivo no S3
+            $url = Storage::disk('s3')->url($path);
+
+            return response()->json(['url' => $url], 200);
+        }
 }
