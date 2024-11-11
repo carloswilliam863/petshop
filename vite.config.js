1f306843b9c0bdaa2018d339c3c5360bb184a9fe
Carlos William <williamxavier0000@gmail.com>
2024-11-11 11:19:43 -0300

    Correções

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        manifest: true,
-        outDir: 'public/build', // Diretório onde os arquivos serão gerados
-    },
     plugins: [
         laravel({
             input: [
